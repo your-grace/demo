03379a07bcb6914cf7f5eed2ff2ad5191eb2c340
liqiubo <1320878281@qq.com>
2023-04-03 16:34:08 +0800

    freemarker

--- src/main/java/com/liqiubo/example/demo/domain/RoleController.java
+++ src/main/java/com/liqiubo/example/demo/domain/RoleController.java
@@ -2,6 +2,7 @@ package com.liqiubo.example.demo.domain;
 
 import net.sf.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +13,15 @@ public class RoleController {
     @Autowired
     private RoleService roleService;
     @GetMapping(value = "/role", produces = {"application/json;charset=UTF-8"})
-    public String getAllRole(){
+    public String getAllRole(Model model) {
         List<TSRole> list = roleService.getAllRoles();
         for (TSRole role : list) {
             System.out.println(role);
         }
         JSONArray jsonArray = JSONArray.fromObject(list);
         System.out.println(jsonArray.toString());
-        return jsonArray.toString();
-
+//        return jsonArray.toString();
+        model.addAttribute("roles", list);
+        return "index";
     }
 }
